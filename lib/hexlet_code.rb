497fa28c48763c5216f524a1babaf74a5d56a6fb
M. Mikhalychev <maxim.myhalichev@yandex.ru>
2023-06-14 22:00:06 +0300

    basic form_for method

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -2,6 +2,9 @@
 
 module HexletCode
   autoload(:VERSION, 'hexlet_code/version.rb')
-
   autoload(:Tag, 'hexlet_code/tag.rb')
+
+  def self.form_for(_entity, params = {}, &_block)
+    Tag.build('form', action: params[:url], method: params[:method] || 'post', **params.except(:url, :method))
+  end
 end
--- spec/hexlet_code_spec.rb
+++ spec/hexlet_code_spec.rb
@@ -4,4 +4,16 @@ RSpec.describe HexletCode do
   it 'has a version number' do
     expect(HexletCode::VERSION).not_to be nil
   end
+
+  describe '.form_for' do
+    subject(:form_for) { described_class.form_for(entity, attributes) }
+
+    let(:user_struct) { Struct.new(:name, keyword_init: true) }
+    let(:entity) { user_struct.new(name: 'rob') }
+    let(:attributes) { { url: '/users', abc: 'hehwtf' } }
+
+    it 'returns html form tag with attributes' do
+      expect(form_for).to eq '<form action="/users" method="post" abc="hehwtf"></form>'
+    end
+  end
 end
